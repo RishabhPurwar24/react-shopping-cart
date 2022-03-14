50ee52256a6806606ae1a1d98c032acf58350cde
RishabhPurwar24 <rishabh15772@gmail.com>
2022-03-14 11:56:24 +0530

    context api setup complete

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Header from "./components/Header";
+import Home from "./components/Home";
+import Cart from "./components/Cart";
 
 function App() {
   return (
     <BrowserRouter>
       <Header />
+      <div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/cart" element={<Cart />} />
+        </Routes>
+      </div>
     </BrowserRouter>
   );
 }
