5370faec3c5c59e77d27b1cf99374237ebf5ab2c
RishabhPurwar24 <rishabh15772@gmail.com>
2022-03-15 17:13:56 +0530

    remove unused ReactDOM

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Header from "./components/Header";
